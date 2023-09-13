a51e4ac5811c55bc48a8ad96e14532e91f1e4171
gankai <geeks.kai@gmail.com>
2023-09-13 21:43:47 +0800

    feat: :art: update logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as React from "react";
 import Image from "next/image";
 import { useTranslations } from "next-intl";
@@ -12,15 +14,25 @@ export const Logo: React.FC<ILogoProps> = (props) => {
   const {} = props;
   const t = useTranslations();
 
+  const imageLoader = ({ src, width, quality }: any) => {
+    // return `https://example.com/${src}?w=${width}&q=${quality || 75}`;
+    return `https://cdn0.iconfinder.com/data/icons/technology-and-support-spot/64/file_convert-512.png?w=${width}&q=${
+      quality || 75
+    }`;
+  };
   return (
     <Link href="/" className="flex mr-32">
       <div className="hidden items-center space-x-2 md:flex">
         <Image
-          src="/logo.jpeg"
+          src="/logo.jpg"
+          // src="/logo.png"
+          // src={imageLoader}
+          // loader={imageLoader}
           className="rounded-md"
+          // src="me.png"
+          alt="Picture of the author"
           width={30}
           height={30}
-          alt="Picture of the author"
         />
         <span className="text-lg">{t("site.title")}</span>
       </div>
